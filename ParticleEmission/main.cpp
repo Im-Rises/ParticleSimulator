ea035688343f3f566da2cced4eda97e7b8508df5
Im-Rises <quentin-morel88@hotmail.com>
2023-01-23 12:25:23 -0500

    Updated cmake

--- ParticleEmission/main.cpp
+++ ParticleEmission/main.cpp
@@ -1,5 +1,22 @@
+#include <glfw/glfw3.h>
+
 int main(int argc, char *argv[])
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Particle Emission", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+    }
     return 0;
 }
